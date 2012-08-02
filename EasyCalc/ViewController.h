ed220aaef761754fcb87eb8589333c9b4da7f614
James Parks <jim.parks3@gmail.com>
2012-08-02 11:51:18 -0500

    Initial Commit

---
+++ EasyCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EasyCalc
+//
+//  Created by iOS Camp on 8/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
